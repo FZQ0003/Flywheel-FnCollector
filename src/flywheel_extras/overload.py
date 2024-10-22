3862b558e63b4effa0d8eceae7944e7cfaf2cc63
FZQ0003 <468403876@qq.com>
2024-10-23 01:41:11 +0800

    feat(overload): MappingOverload (delete PredicateOverload)

--- src/flywheel_extras/overload.py
+++ src/flywheel_extras/overload.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import Callable, Mapping
 from typing import Any
 
 from flywheel.overloads import SimpleOverload
 
 
-class PredicateOverload(SimpleOverload):
-    predicate: Callable[[str, Any], Any]  # (arg_name, call_value) -> collect_value
+class MappingOverload(SimpleOverload):
+    map_func: Callable[[Any], Any]  # (call_value) -> collect_value
 
-    def __init__(self, name: str, predicate: Callable[[str, Any], Any]):
+    def __init__(self, name: str, mapping: Callable[[Any], Any] | Mapping[Any, Any]):
         super().__init__(name)
-        self.predicate = predicate
+        self.map_func = mapping if isinstance(mapping, Callable) else lambda _: mapping.get(_, None)
 
     def harvest(self, scope: dict, call_value: Any) -> dict[Callable, None]:
-        return super().harvest(scope, self.predicate(self.name, call_value))
+        return super().harvest(scope, self.map_func(call_value))
