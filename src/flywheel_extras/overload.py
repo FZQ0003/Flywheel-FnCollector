e5a6b607d651789088640c9299c00b5a42e23712
FZQ0003 <468403876@qq.com>
2024-10-23 13:06:57 +0800

    chore(overload): Change __init__()

--- src/flywheel_extras/overload.py
+++ src/flywheel_extras/overload.py
@@ -14,22 +14,24 @@ def _dummy_predicate(collect_value, call_value):
 
 
 class MappingOverload(SimpleOverload):
-    map_func: Callable[[Any], Any]  # (call_value) -> collect_value
+    map_func: Callable[[Any], Any] = _dummy_map_func  # (call_value) -> collect_value
 
-    def __init__(self, name: str, mapping: Callable[[Any], Any] | Mapping[Any, Any] = _dummy_map_func):
+    def __init__(self, name: str, mapping: Callable[[Any], Any] | Mapping[Any, Any] | None = None):
         super().__init__(name)
-        self.map_func = mapping if isinstance(mapping, Callable) else lambda _: mapping.get(_, None)
+        if mapping:
+            self.map_func = mapping if isinstance(mapping, Callable) else lambda _: mapping.get(_, None)
 
     def harvest(self, scope: dict, call_value: Any) -> dict[Callable, None]:
         return super().harvest(scope, self.map_func(call_value))
 
 
 class PredicateOverload(SimpleOverload):
-    predicate: Callable[[Any, Any], bool]  # (collect_value, call_value) -> bool
+    predicate: Callable[[Any, Any], bool] = _dummy_predicate  # (collect_value, call_value) -> bool
 
-    def __init__(self, name: str, predicate: Callable[[Any, Any], bool] = _dummy_predicate):
+    def __init__(self, name: str, predicate: Callable[[Any, Any], bool] | None = None):
         super().__init__(name)
-        self.predicate = predicate
+        if predicate:
+            self.predicate = predicate
 
     def harvest(self, scope: dict, call_value: Any) -> dict[Callable, None]:
         result = {}
